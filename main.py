461fb9bfd1529223786d263f9555a0fbbda67755
Nick <ayresn@phys.ethz.ch>
2022-06-21 19:21:26 +0200

    test for connectedness

--- main.py
+++ main.py
@@ -6,13 +6,19 @@ Created on Tue Jun 21 17:43:34 2022
 """
 
    
-from words import wp,new_words4#,words4
+from words import wp,new_words4,words4
 import networkx as nx
 G = nx.Graph()
 
-if True:
+#print("removed words are:", [w for w in words4 if w not in new_words4])
+new_dict = False
+
+if new_dict:
     #test with modified words list
+    new_words4.append("taco") #hotfix for issue in nathans dict
     words4 = new_words4
+else:
+    from words import words4
 
 G.add_nodes_from(words4)
 
@@ -26,12 +32,24 @@ for i in range(len(words4)):
                 diffs = diffs+1
         if diffs==3:
             G.add_edge(words4[i], words4[j])
-        
+
+#solve test problem        
 print([p for p in nx.all_shortest_paths(G,"solo","duet")])
 
+#determine the connected subgraphs - 
+subgraphs = list(nx.connected_components(G))
 
-#for pair in wp:
-#    print([p for p in nx.all_shortest_paths(G,pair[0],pair[1])])
+sorted_subgraphs = sorted(subgraphs,key=len,reverse=True)
+print(len(sorted_subgraphs[0]), " out of ", len(words4), " words are connected to the main dictionary")
+print("the following sets of words are unreachable from the main dictionary:")
+print(sorted_subgraphs[1:])
+
+for pair in wp:
+    #print(pair)
+    try:
+        sols = [p for p in nx.all_shortest_paths(G,pair[0],pair[1])]
+    except:
+        print("Yikes - problem for ", pair)
     
     
     
